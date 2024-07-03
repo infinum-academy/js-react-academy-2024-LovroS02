eba819c08092721fb8623a4eb63a34afed2cc0c3
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-03 09:50:50 +0200

    Exercise1 - added functions for rendering reviews and ratings from list, added style for reviews list

---
+++ static/index.js
@@ -0,0 +1,33 @@
+let reviewsList = [
+  {
+    review: "Cool, cool, cool, cool. No doubt, no doubt, no doubt.",
+    rating: "5 / 5",
+  },
+];
+
+function renderReviewsList() {
+  const reviewRatingListElement = document.getElementById(
+    "reviews-ratings-list"
+  );
+  reviewRatingListElement.innerHTML = "";
+  reviewsList.forEach((review) => {
+    reviewRatingListElement.appendChild(createReview(review));
+  });
+}
+
+function createReview(review) {
+  const reviewRatingElement = document.createElement("div");
+  reviewRatingElement.id = "review-rating-item";
+
+  const reviewElement = document.createElement("span");
+  reviewElement.textContent = review.review;
+  reviewRatingElement.appendChild(reviewElement);
+
+  const ratingElement = document.createElement("span");
+  ratingElement.textContent = review.rating;
+  reviewRatingElement.appendChild(ratingElement);
+
+  return reviewRatingElement;
+}
+
+renderReviewsList();
