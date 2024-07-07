4a67332d01d6d6a6ef3a5762441c82de94bb2cdd
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-07 23:01:00 +0200

    Added styling to ReviewForm component

--- tv_show/src/components/features/shows/ReviewForm/ReviewForm.tsx
+++ tv_show/src/components/features/shows/ReviewForm/ReviewForm.tsx
@@ -25,11 +25,37 @@ export const ReviewForm = ({ addShowReview }: ReviewFormProps) => {
   };
 
   return (
-    <Flex gap={4}>
-      <Heading size="md">Reviews</Heading>
-      <Input id="comment-input" size="md" placeholder="Add review"></Input>
-      <Input id="rating-input" size="md" placeholder="Add rating"></Input>
-      <Button variant="solid" colorScheme="white" onClick={onClickHandler}>
+    <Flex direction="column" gap={4} width="60%">
+      <Heading mt="10px" borderRadius="10px" color="white" size="lg">
+        Reviews
+      </Heading>
+      <Input
+        height="100px"
+        borderRadius="10px"
+        bg="white"
+        color="black"
+        id="comment-input"
+        size="md"
+        placeholder="Add review"
+        padding="0px 0px 50px 10px"
+      ></Input>
+      <Input
+        width="30%"
+        borderRadius="10px"
+        bg="white"
+        color="black"
+        id="rating-input"
+        size="md"
+        placeholder="Add rating"
+      ></Input>
+      <Button
+        width="15%"
+        borderRadius="20px"
+        variant="solid"
+        bg="white"
+        color="black"
+        onClick={onClickHandler}
+      >
         Post
       </Button>
     </Flex>
