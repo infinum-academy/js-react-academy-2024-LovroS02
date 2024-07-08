c7ab267659e26ac20183b43246e9c3cfd84a6143
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-08 14:34:43 +0200

    Added CustomRatingInput to ReviewForm and changed styling of CustomRatingInput

--- tv_show/src/components/features/shows/ReviewForm/ReviewForm.tsx
+++ tv_show/src/components/features/shows/ReviewForm/ReviewForm.tsx
@@ -1,67 +1,56 @@
-import { IReview } from "@/typings/review";
-import { Flex, Heading, Input, Button } from "@chakra-ui/react";
+import { CustomRatingInput } from '@/components/shared/input/CustomRatingInput/CustomRatingInput';
+import { IReview } from '@/typings/review';
+import { Flex, Heading, Input, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface ReviewFormProps {
-  addShowReview: (review: IReview) => void;
+	addShowReview: (review: IReview) => void;
 }
 
 export const ReviewForm = ({ addShowReview }: ReviewFormProps) => {
-  const onClickHandler = () => {
-    const commentInput = document.getElementById(
-      "comment-input"
-    ) as HTMLInputElement;
-    const ratingInput = document.getElementById(
-      "rating-input"
-    ) as HTMLInputElement;
+	const [internalValue, setInternalValue] = useState(0);
 
-    if (!commentInput.value || !ratingInput.value) {
-      return
-    }
+	const onClickHandler = () => {
+		const commentInput = document.getElementById('comment-input') as HTMLInputElement;
+		const ratingInput = internalValue;
 
-    const newReview: IReview = {
-      comment: commentInput.value,
-      rating: parseInt(ratingInput.value),
-      email: "test@gmail.com",
-      avatar: "",
-    };
+		if (!commentInput.value || !ratingInput) {
+			return;
+		}
 
-    addShowReview(newReview);
-  };
+		const newReview: IReview = {
+			comment: commentInput.value,
+			rating: ratingInput,
+			email: 'test@gmail.com',
+			avatar: '',
+		};
 
-  return (
-    <Flex direction="column" gap={4} width="60%">
-      <Heading mt="10px" borderRadius="10px" color="white" size="lg">
-        Reviews
-      </Heading>
-      <Input
-        height="100px"
-        borderRadius="10px"
-        bg="white"
-        color="black"
-        id="comment-input"
-        size="md"
-        placeholder="Add review"
-        padding="0px 0px 50px 10px"
-      ></Input>
-      <Input
-        width="30%"
-        borderRadius="10px"
-        bg="white"
-        color="black"
-        id="rating-input"
-        size="md"
-        placeholder="Add rating"
-      ></Input>
-      <Button
-        width="10%"
-        borderRadius="20px"
-        variant="solid"
-        bg="white"
-        color="black"
-        onClick={onClickHandler}
-      >
-        Post
-      </Button>
-    </Flex>
-  );
+		addShowReview(newReview);
+	};
+
+	const onChange = (index: number) => {
+		setInternalValue(index);
+	};
+
+	return (
+		<Flex direction="column" gap={4} width="60%">
+			<Heading mt="10px" borderRadius="10px" color="white" size="lg">
+				Reviews
+			</Heading>
+			<Input
+				height="100px"
+				borderRadius="10px"
+				bg="white"
+				color="black"
+				id="comment-input"
+				size="md"
+				placeholder="Add review"
+				padding="0px 0px 50px 10px"
+			></Input>
+			<CustomRatingInput label="Rating" value={internalValue} onChange={onChange} />
+			<Button width="10%" borderRadius="20px" variant="solid" bg="white" color="black" onClick={onClickHandler}>
+				Post
+			</Button>
+		</Flex>
+	);
 };
--- tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
+++ tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
@@ -1,6 +1,5 @@
 import { StarIcon } from '@chakra-ui/icons';
 import { Flex, Input, useStatStyles } from '@chakra-ui/react';
-import { useState } from 'react';
 
 interface CustomRatingInputProps {
 	label: string;
@@ -10,18 +9,20 @@ interface CustomRatingInputProps {
 
 export const CustomRatingInput = ({ label, value = 0, onChange }: CustomRatingInputProps) => {
 	return (
-		<Flex direction="row" gap={1}>
-			<Input color="white" value={label} variant="unstyled" size="md" />
-			{Array.from(Array(5)).map((_, index) => {
-				return (
-					<StarIcon
-						key={index}
-						boxSize={6}
-						color={index + 1 <= value ? 'gold' : 'gray.300'}
-						onClick={() => onChange(index + 1)}
-					/>
-				);
-			})}
+		<Flex direction="row" alignItems="center">
+			<Input color="white" value={label} variant="unstyled" size="lg" maxW="15%"/>
+			<Flex direction="row" gap={1}>
+				{Array.from(Array(5)).map((_, index) => {
+					return (
+						<StarIcon
+							key={index}
+							boxSize={6}
+							color={index + 1 <= value ? 'gold' : 'gray.300'}
+							onClick={() => onChange(index + 1)}
+						/>
+					);
+				})}
+			</Flex>
 		</Flex>
 	);
 };
