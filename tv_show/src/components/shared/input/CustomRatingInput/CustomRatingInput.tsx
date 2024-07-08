10d204a08357de7738d1a2e2683872a257b81585
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-08 15:25:07 +0200

    Added cursor styling on CustomRatingInput and add hover styling

--- tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
+++ tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
@@ -1,5 +1,6 @@
 import { StarIcon } from '@chakra-ui/icons';
-import { Flex, Input, useStatStyles } from '@chakra-ui/react';
+import { color, Flex, Input } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface CustomRatingInputProps {
 	label: string;
@@ -8,16 +9,37 @@ interface CustomRatingInputProps {
 }
 
 export const CustomRatingInput = ({ label, value, onChange }: CustomRatingInputProps) => {
+	const [hoveredIndex, setHoveredIndex] = useState(0);
+
+	const onHover = (index: number) => {
+		setHoveredIndex(index);
+	};
+
+	const setColor = (index: number) => {
+		if (hoveredIndex) {
+			if (index <= hoveredIndex) {
+				return 'gold';
+			}
+			return 'gray.300';
+		}
+		if (index <= value) {
+			return 'gold';
+		}
+		return 'gray.300';
+	};
+
 	return (
 		<Flex direction="row" alignItems="center">
 			<Input color="white" value={label} variant="unstyled" size="lg" maxW="15%" />
-			<Flex direction="row" gap={1}>
+			<Flex direction="row" gap={1} onMouseLeave={() => setHoveredIndex(0)}>
 				{Array.from(Array(5)).map((_, index) => {
 					return (
 						<StarIcon
 							key={index}
 							boxSize={6}
-							color={index + 1 <= value ? 'gold' : 'gray.300'}
+							color={setColor(index + 1)}
+							onMouseOver={() => onHover(index + 1)}
+							cursor={label === 'Rating' ? 'pointer' : 'default'}
 							onClick={() => onChange(index + 1)}
 						/>
 					);
