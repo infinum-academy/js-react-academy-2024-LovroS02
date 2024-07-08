f7c81384b7b98c39657f6cd45be20bcd56af7fb7
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-08 14:42:57 +0200

    Added CustomRatingInput to ReviewItem

--- tv_show/src/components/features/review/ReviewItem/ReviewItem.tsx
+++ tv_show/src/components/features/review/ReviewItem/ReviewItem.tsx
@@ -1,36 +1,24 @@
-import { IReview } from "@/typings/review";
-import { Flex, Text, Avatar, Button } from "@chakra-ui/react";
+import { CustomRatingInput } from '@/components/shared/input/CustomRatingInput/CustomRatingInput';
+import { IReview } from '@/typings/review';
+import { Flex, Text, Avatar, Button } from '@chakra-ui/react';
 
 interface ReviewItemProps {
-  review: IReview;
-  onDelete: (review: IReview) => void;
+	review: IReview;
+	onDelete: (review: IReview) => void;
 }
 
 export const ReviewItem = ({ review, onDelete }: ReviewItemProps) => {
-  return (
-    <Flex
-      bg="blue"
-      direction="column"
-      padding="20px"
-      gap={4}
-      borderRadius="10px"
-    >
-      <Flex direction="row" justifyContent="space-between" alignItems="center">
-        <Text>{review.email}</Text>
-        <Avatar src={review.avatar} bg="darkblue"></Avatar>
-      </Flex>
-      <Text>{review.comment}</Text>
-      <Text>{review.rating} / 5</Text>
-      <Button
-        width="15%"
-        bg="white"
-        color="black"
-        variant="solid"
-        onClick={() => onDelete(review)}
-        borderRadius="20px"
-      >
-        Remove
-      </Button>
-    </Flex>
-  );
+	return (
+		<Flex bg="blue" direction="column" padding="20px" gap={4} borderRadius="10px">
+			<Flex direction="row" justifyContent="space-between" alignItems="center">
+				<Text>{review.email}</Text>
+				<Avatar src={review.avatar} bg="darkblue"></Avatar>
+			</Flex>
+			<Text>{review.comment}</Text>
+			<CustomRatingInput label={review.rating + ' / 5'} value={review.rating} onChange={() => {}} />
+			<Button width="15%" bg="white" color="black" variant="solid" onClick={() => onDelete(review)} borderRadius="20px">
+				Remove
+			</Button>
+		</Flex>
+	);
 };
--- tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
+++ tv_show/src/components/shared/input/CustomRatingInput/CustomRatingInput.tsx
@@ -7,10 +7,10 @@ interface CustomRatingInputProps {
 	onChange: (index: number) => void;
 }
 
-export const CustomRatingInput = ({ label, value = 0, onChange }: CustomRatingInputProps) => {
+export const CustomRatingInput = ({ label, value, onChange }: CustomRatingInputProps) => {
 	return (
 		<Flex direction="row" alignItems="center">
-			<Input color="white" value={label} variant="unstyled" size="lg" maxW="15%"/>
+			<Input color="white" value={label} variant="unstyled" size="lg" maxW="15%" />
 			<Flex direction="row" gap={1}>
 				{Array.from(Array(5)).map((_, index) => {
 					return (
