0cf039969ff02206c6b0d2633869882690469ba2
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-09 15:58:28 +0200

    Created ShowCard component

---
+++ tv_show/src/components/shared/shows/ShowCard/ShowCard.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { IShow } from '@/typings/show';
+import { StarIcon } from '@chakra-ui/icons';
+import { Heading, Text, Image, Card, CardBody, Flex } from '@chakra-ui/react';
+
+interface IShowCardProps {
+	show: IShow;
+}
+
+export const ShowCard = ({ show }: IShowCardProps) => {
+	return (
+		<Card maxW="2xs" borderRadius="20px">
+			<Image src={show.imageUrl} fallbackSrc="https://fakeimg.pl/600x400" alt={show.title + ' image'} borderTopRadius="20px" />
+			<Heading size="md" fontFamily="bold" padding="15px 0px 0px 15px">
+				{show.title}
+			</Heading>
+			<Flex alignItems="center" direction="row" mb={4} padding="5px 0px 0px 15px">
+                <StarIcon boxSize={3} color="darkblue" />
+                <Text fontSize="sm" pl={1}>{show.averageRating + "/5"}</Text>
+            </Flex>
+		</Card>
+	);
+};
