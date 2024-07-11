21b10e5be822d10cedbdf8551f8618f1f9f39a57
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-11 13:05:16 +0200

    Fixed ShowCard.test

--- tv_show/src/components/shared/shows/ShowCard/ShowCard.test.tsx
+++ tv_show/src/components/shared/shows/ShowCard/ShowCard.test.tsx
@@ -2,42 +2,47 @@ import { render, screen } from '@testing-library/react';
 import { ShowCard } from './ShowCard';
 
 describe('ShowCard', () => {
-	const mockShow = {
+	const mockShow1 = {
 		title: 'test',
 		description: 'test test test',
 		id: '100',
-		image_url: 'test.png',
-		average_rating: 0,
+		image_url: 'https://fakeimg.pl/600x400',
+		average_rating: 0 || null || undefined,
 		no_of_reviews: 0,
 	};
 
-	it('should contain Image element with provided url', () => {
-		render(<ShowCard show={mockShow} />);
+	const mockShow2 = {
+		title: 'test',
+		description: 'test test test',
+		id: '100',
+		image_url: 'https://fakeimg.pl/600x400',
+		average_rating: 4,
+		no_of_reviews: 0,
+	};
+
+	it('should contain Image element with url', () => {
+		render(<ShowCard show={mockShow1} />);
 
 		const image = screen.getByRole('img');
 		expect(image).toBeInTheDocument();
+		expect(image).toHaveAttribute('src', mockShow1.image_url);
 	});
 
 	it('should render show title', () => {
-		render(<ShowCard show={mockShow} />);
+		render(<ShowCard show={mockShow1} />);
 
-		const heading = screen.getByRole('heading');
-		expect(heading).toBeInTheDocument();
-		expect(heading).toHaveTextContent(mockShow.title);
+		expect(screen.getByText(mockShow1.title)).toBeInTheDocument();
+	});
+
+	it('should render show average rating as no rating', () => {
+		render(<ShowCard show={mockShow1} />);
+
+		expect(screen.getByText('no rating')).toBeInTheDocument();
 	});
 
 	it('should render show average rating', () => {
-		render(<ShowCard show={mockShow} />);
+		render(<ShowCard show={mockShow2} />);
 
-		const checkAverageRating = (rating?: number) => {
-			if (rating !== undefined && rating !== 0 && rating !== null) {
-				return rating + ' / 5';
-			}
-			return 'no rating';
-		};
-
-		const text = screen.getByRole('paragraph');
-		expect(text).toBeInTheDocument();
-		expect(text).toHaveTextContent(checkAverageRating(mockShow.average_rating));
+		expect(screen.getByText(mockShow2.average_rating + '/5')).toBeInTheDocument();
 	});
 });
