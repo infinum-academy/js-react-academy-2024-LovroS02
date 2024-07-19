d821a6aa73b9966f7c563bf7968d10b484c5cb1b
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-19 12:52:44 +0200

    Created test for ReviewList with mocked ReviewItem

---
+++ tv_show/src/components/features/review/ReviewList/ReviewList.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { ReviewItem } from '../ReviewItem/ReviewItem';
+import { ReviewList } from './ReviewList';
+
+jest.mock('../ReviewItem/ReviewItem', () => {
+    return {
+        ReviewItem: jest.fn().mockReturnValue(null),
+    }
+})
+
+describe('ReviewList', () => {
+    const mockReviews = [
+        {
+            rating: 1,
+            comment: 'test test test',
+            id: '10000',
+            show_id: '100',
+            user: { id: '1', email: 'test@gmail.com', image_url: '' },
+        },
+        {
+            rating: 5,
+            comment: 'test test test',
+            id: '10001',
+            show_id: '101',
+            user: { id: '2', email: 'test2@gmail.com', image_url: '' },
+        }
+    ]
+
+	it('should render ReviewItem', () => {
+        render(<ReviewList reviewList={mockReviews} />)
+
+		mockReviews.forEach((review, index) => {
+			expect(ReviewItem).toHaveBeenNthCalledWith(index + 1, { review }, expect.anything());
+		});
+	});
+});
--- tv_show/src/components/shared/shows/ShowsList/ShowsList.test.tsx
+++ tv_show/src/components/shared/shows/ShowsList/ShowsList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { ShowsList } from './ShowsList';
 import { ShowCard } from '../ShowCard/ShowCard';
 
