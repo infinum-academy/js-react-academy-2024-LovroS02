567a327c84fafd96e86e533c47e2c70fa025094f
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-22 13:12:00 +0200

    Added colors fontSizes and fontWeights to foundations folder, replaced fontSizes with semantic tokens

--- tv_show/src/styles/theme/theme.ts
+++ tv_show/src/styles/theme/theme.ts
@@ -1,37 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 import '@fontsource/roboto';
-
-const colors = {
-	white: '#ffffff',
-	lightPurple: '#8D5CE5',
-	purple_2: '#371687',
-	darkPurple: '#1B004C',
-	error: '#FF2498',
-	black: '#000000',
-};
+import colors from './foundations/colors';
+import fontSizes from './foundations/fontSizes';
+import fontWeights from './foundations/fontWeights';
 
 const fonts = {
 	body: 'Roboto, sans-serif',
 	heading: 'Roboto, sans-serif',
-};
-
-const fontSizes = {
-	headline: {
-		web: '40px',
-		mobile: '24px',
-	},
-	title: '24px',
-	body: { web: '20px', mobile: '14px' },
-	subtitle: '18px',
-	smallCaption: { web: '16px', mobile: '14px' },
-	buttonOrCaption: '14px',
-	note: '12px',
-};
-
-const fontWeights = {
-	regular: 400,
-	bold: 700,
-	lightBold: 500,
 };
 
 const theme = extendTheme({
