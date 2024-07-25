70b73cf02129767e217f25734c97d0d98f96e950
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-25 15:05:33 +0200

    Created PickerProgress component which shows progress bar, added buttons and context to Picker component

--- tv_show/src/components/features/picker/Picker/Picker.tsx
+++ tv_show/src/components/features/picker/Picker/Picker.tsx
@@ -2,6 +2,7 @@
 
 import {
 	Button,
+	Flex,
 	Modal,
 	ModalBody,
 	ModalContent,
@@ -10,24 +11,45 @@ import {
 	ModalOverlay,
 	useDisclosure,
 } from '@chakra-ui/react';
+import { PickerProgress } from './components/PickerProgress';
+import { createContext, useState } from 'react';
+
+interface IPickerContext {
+	currentStep: number;
+}
+
+export const PickerContext = createContext<IPickerContext>({} as IPickerContext);
 
 export const Picker = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [currentStep, setCurrentStep] = useState(0);
 
 	return (
 		<>
-			<Button onClick={onOpen} bg={{ base: 'purple.400', sm: 'purple.700' }}>
-				Picker
-			</Button>
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay>
-					<ModalContent>
-						<ModalHeader></ModalHeader>
-						<ModalBody></ModalBody>
-						<ModalFooter></ModalFooter>
-					</ModalContent>
-				</ModalOverlay>
-			</Modal>
+			<PickerContext.Provider value={{ currentStep }}>
+				<Button onClick={onOpen} bg={{ base: 'purple.400', sm: 'purple.700' }}>
+					Picker
+				</Button>
+				<Modal isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay>
+						<ModalContent>
+							<ModalHeader>Picker</ModalHeader>
+							<ModalBody></ModalBody>
+							<ModalFooter>
+								<Flex direction="column" width="100%" gap={3}>
+									<PickerProgress />
+									<Flex width="100%" justifyContent="space-between">
+										<Button hidden={currentStep === 0} onClick={() => setCurrentStep(currentStep - 1)}>
+											Previous
+										</Button>
+										<Button onClick={() => setCurrentStep(currentStep + 1)}>Next</Button>
+									</Flex>
+								</Flex>
+							</ModalFooter>
+						</ModalContent>
+					</ModalOverlay>
+				</Modal>
+			</PickerContext.Provider>
 		</>
 	);
 };
