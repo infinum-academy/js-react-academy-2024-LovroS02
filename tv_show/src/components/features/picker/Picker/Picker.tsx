1c19645fcb70fa1484238dedced48ba88dd638d3
LovroSvenda <lovro.svenda1@gmail.com>
2024-07-25 16:07:00 +0200

    Created new service getShows which returns array of shows, added states for rounds in Picker component

--- tv_show/src/components/features/picker/Picker/Picker.tsx
+++ tv_show/src/components/features/picker/Picker/Picker.tsx
@@ -14,10 +14,23 @@ import {
 import { PickerProgress } from './components/PickerProgress';
 import { createContext, useState } from 'react';
 import { PickerButtons } from './components/PickerButtons';
+import { fetcher } from '@/fetchers/fetcher';
+import { swrKeys } from '@/fetchers/swrKeys';
+import useSWR from 'swr';
+import { IShow } from '@/typings/show';
+import { getRoundShows } from '@/services/getShows';
+
+interface IShowsListResponse {
+	shows: Array<IShow>;
+}
 
 interface IPickerContext {
 	currentStep: number;
 	setCurrentStep: (newStep: number) => void;
+	currentRound: number;
+	setCurrentRound: (newRound: number) => void;
+	roundShows: Array<IShow>;
+	setRoundShows: (newShows: Array<IShow>) => void;
 }
 
 export const PickerContext = createContext<IPickerContext>({} as IPickerContext);
@@ -25,10 +38,20 @@ export const PickerContext = createContext<IPickerContext>({} as IPickerContext)
 export const Picker = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [currentStep, setCurrentStep] = useState(0);
+	const [currentRound, setCurrentRound] = useState(1);
+	const { data } = useSWR<IShowsListResponse>(swrKeys.allShows, fetcher);
+
+	if (!data) {
+		return null;
+	}
+
+	const [roundShows, setRoundShows] = useState(getRoundShows(8, data.shows));
 
 	return (
 		<>
-			<PickerContext.Provider value={{ currentStep, setCurrentStep }}>
+			<PickerContext.Provider
+				value={{ currentStep, setCurrentStep, currentRound, setCurrentRound, roundShows, setRoundShows }}
+			>
 				<Button onClick={onOpen} bg={{ base: 'purple.400', sm: 'purple.700' }}>
 					Picker
 				</Button>
---
+++ tv_show/src/components/features/picker/Picker/components/PickerShowStep.tsx
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { PickerContext } from "../Picker";
+
+export const PickerShowStep = () => {
+    const { roundShows, currentRound } = useContext(PickerContext);
+
+	return;
+};
